30e4fcb0304fdb6406786702f00788aca8281fb7
Malvoon <smelvoon@gmail.com>
2024-07-25 01:06:18 +0200

    feat: affichage du résultat et de l'opération effectuée

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,39 +1,69 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const Calculator = () => {
+  const [changed, setChanged] = useState(false);
+  const [lastDisplay, setLastDisplay] = useState('');
   const [display, setDisplay] = useState('');
 
-  function calculate() {
-    try {
-      const output = display.replace(/x/g, '*').replace(/÷/g, '/').replace(/%/g, '/100*');
-      setDisplay(eval(output));
-    } catch (error) {
-      setDisplay('error');
+  function addNumber(x: string) {
+    if (display === '0') {
+      setDisplay(x);
     }
+    else {
+      setDisplay(display + x);
+    }
+  }
+
+  function operator(op: string) {
+    if (op === '-') {
+      setDisplay(op);
+    }
+    else if (op === '%') {
+      setLastDisplay(display + op);
+      setDisplay(eval(display + '/100'));
+    }
+    else if (display !== '') {
+      setLastDisplay(display + op);
+      setDisplay('');
+    }
+  }
+
+  function compute() {
+    const output = lastDisplay + display;
+    setLastDisplay(output);
+    setDisplay(eval(output.replace('x', '*').replace('÷', '/').replace('%', '/100')));
+  }
+
+  function clean() {
+    setLastDisplay('');
+    setDisplay('');
   }
 
   return (
     <div className="calculator">
-      <div className="display">{display}</div>
-      <button className="clear" onClick={() => setDisplay('')}>AC</button>
+      <div className="screen">
+        <div className="lastDisplay">{lastDisplay}</div>
+        <div className="display">{display}</div>
+      </div>
+      <button className="clear" onClick={() => clean()}>AC</button>
       <button className="delete" onClick={() => setDisplay(display.slice(0, -1))}>DEL</button>
-      <button className="operator" onClick={() => setDisplay(display + '%')}>%</button>
-      <button className="operator" onClick={() => setDisplay(display + '÷')}>÷</button>
-      <button className="number" onClick={() => setDisplay(display + '7')}>7</button>
-      <button className="number" onClick={() => setDisplay(display + '8')}>8</button>
-      <button className="number" onClick={() => setDisplay(display + '9')}>9</button>
-      <button className="operator" onClick={() => setDisplay(display + 'x')}>x</button>
-      <button className="number" onClick={() => setDisplay(display + '4')}>4</button>
-      <button className="number" onClick={() => setDisplay(display + '5')}>5</button>
-      <button className="number" onClick={() => setDisplay(display + '6')}>6</button>
-      <button className="operator" onClick={() => setDisplay(display + '-')}>-</button>
-      <button className="number" onClick={() => setDisplay(display + '1')}>1</button>
-      <button className="number" onClick={() => setDisplay(display + '2')}>2</button>
-      <button className="number" onClick={() => setDisplay(display + '3')}>3</button>
-      <button className="operator" onClick={() => setDisplay(display + '+')}>+</button>
-      <button className="number" onClick={() => setDisplay(display + '0')}>0</button>
-      <button className="number" onClick={() => setDisplay(display + '.')}>.</button>
-      <button className="equal" onClick={() => calculate()}>=</button>
+      <button className="operator" onClick={() => { operator('%') }}>%</button>
+      <button className="operator" onClick={() => operator('÷')}>÷</button>
+      <button className="number" onClick={() => addNumber('7')}>7</button>
+      <button className="number" onClick={() => addNumber('8')}>8</button>
+      <button className="number" onClick={() => addNumber('9')}>9</button>
+      <button className="operator" onClick={() => operator('x')}>x</button>
+      <button className="number" onClick={() => addNumber('4')}>4</button>
+      <button className="number" onClick={() => addNumber('5')}>5</button>
+      <button className="number" onClick={() => addNumber('6')}>6</button>
+      <button className="operator" onClick={() => operator('-')}>-</button>
+      <button className="number" onClick={() => addNumber('1')}>1</button>
+      <button className="number" onClick={() => addNumber('2')}>2</button>
+      <button className="number" onClick={() => addNumber('3')}>3</button>
+      <button className="operator" onClick={() => operator('+')}>+</button>
+      <button className="number" onClick={() => addNumber('0')}>0</button>
+      <button className="number" onClick={() => addNumber('.')}>.</button>
+      <button className="equal" onClick={() => compute()}>=</button>
     </div>
   );
 };
